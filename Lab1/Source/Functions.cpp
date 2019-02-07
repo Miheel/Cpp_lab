6bdff683e3463dd63eb0377fb275cfda5fdc3a94
Miheel <mikaelleuf@gmail.com>
2019-02-08 00:44:59 +0100

    ez game complete

--- Lab1/Source/Functions.cpp
+++ Lab1/Source/Functions.cpp
@@ -1,19 +1,19 @@
 #include <iostream>
-#include "Functions.hpp"
 #include <stdlib.h>
 #include <time.h>
+#include "Functions.hpp"
 
 void ResetMap(Map<std::uint16_t> &map)
 {
     // Local helper function to reset all non treasure positions on the map to the maximum value
     // so that updateposition can then give them their correct values based on the remaining treasures
-    for (long long unsigned int i = 0; i < map.size(); i++)
+    for (std::uint16_t yPos = 0; yPos < map.size(); yPos++)
     {
-        for (long long unsigned int j = 0; j < map.size(); j++)
+        for (std::uint16_t xPos = 0; xPos < map[0].size(); xPos++)
         {
-            if (map[j][i] != 0)
+            if (map[yPos][xPos] != 0)
             {
-                map[j][i] = map.size();
+                map[yPos][xPos] = static_cast<std::uint16_t>(map.size());
             }
         }
     }
@@ -22,82 +22,129 @@ void ResetMap(Map<std::uint16_t> &map)
 void UpdateFromPosition(Map<std::uint16_t> &map, std::uint16_t xPos, std::uint16_t yPos)
 {
     // Local helper function to update the map based on a specific position indicated by xPos and yPos
-    for (long long unsigned int i = 0; i < map.size(); i++)
+	int lenPoint = 0;
+	int x1 = 0;
+	int y1= 0;
+    int x2 = 0;
+	int y2 = 0;
+
+    for (std::uint16_t i = 0; i < map.size(); i++)
     {
-        for (long long unsigned int j = 0; j < map.size(); j++)
-        {
-            if (map[j][i] != 0)
-            {
-            }
+        for (std::uint16_t j = 0; j < map[0].size(); j++)
+        {			    
+			if (map[i][j] != 0)
+			{
+				//Finds the distance between the treasures and all the other point with the distance folmula
+                x1 = i - yPos;
+                y1 = j - xPos;
+
+    			x2 = (int)std::pow(x1, 2);
+    			y2 = (int)std::pow(y1, 2);
+
+    			lenPoint = (int)std::sqrt(x2 + y2);
+
+				if(map[i][j] > lenPoint)
+				{						
+					map[i][j] = lenPoint;
+				}
+			}		
         }
     }
 }
 
 void CreateMap(Map<std::uint16_t> &map, Map<bool> &takenSpaces, std::uint16_t width, std::uint16_t height)
 {
-    srand(time(NULL));
-
-    for (long long unsigned int i = 0; i < width; i++)
+	//Creates a 2d vector based on with and height
+	map.resize(height, Column<std::uint16_t>(width, 1));
+	takenSpaces.resize(height, Column<bool>(width, false));
+    /*for (std::uint16_t i = 0; i < height; i++)
     {
         map.emplace_back();
         takenSpaces.emplace_back();
 
-        for (long long unsigned int j = 0; j < height; j++)
+        for (std::uint16_t j = 0; j < width; j++)
         {
             map.back().push_back(1);
             takenSpaces.back().push_back(false);
         }
-    }
+    }*/
 
-    for (int i = 0; i < 3; i++)
+	//Places 3 treasures on the map 
+    srand(static_cast<unsigned>(time(NULL)));
+    for (int i = 0; i < 3;)
     {
         int x = rand() % width;
         int y = rand() % height;
-        map[y][x] = 0;
+
+		while (map[y][x] != 0)
+		{
+			map[y][x] = 0;
+			i++;
+		}
     }
+
     UpdateMap(map);
 }
 
 void UpdateMap(Map<std::uint16_t> &map)
 {
+	//Finds the locatios of the treasures and calls for UpdateFromPosition at the pos of the treasures
     ResetMap(map);
-    for (long long unsigned int xPos = 0; xPos < map.size(); xPos++)
+
+    for (std::uint16_t yPos = 0; yPos < map.size(); yPos++)
     {
-        for (long long unsigned int yPos = 0; yPos < map.size(); yPos++)
+        for (std::uint16_t xPos = 0; xPos < map[0].size(); xPos++)
         {
             if (map[yPos][xPos] == 0)
             {
                 UpdateFromPosition(map, xPos, yPos);
-            }
+			}
         }
     }
 }
 
+const void erase()
+{
+#if defined _WIN32
+	system("cls");
+#else
+	system("clear");
+#endif
+}
+
 void PrintMap(Map<std::uint16_t> &map, Map<bool> &takenSpaces)
 {
-    for (long long unsigned int i = 0; i < map.size(); i++)
+	//clears the screen and print out the map
+	erase();
+
+    for (std::uint16_t yPos = 0; yPos < map.size(); yPos++)
     {
-        for (long long unsigned int j = 0; j < map.size(); j++)
+        for (std::uint16_t xPos = 0; xPos < map[0].size(); xPos++)
         {
-            if (takenSpaces[j][i] == true)
+            if (takenSpaces[yPos][xPos] == true)
             {
-                std::cout << map[j][i] << " ";
+                std::cout << " " << map[yPos][xPos] << " ";
             }
+
             else
             {
                 std::cout << " * ";
             }
         }
+
         std::cout << std::endl;
     }
 }
 
 Coordinate InputCoordinates()
 {
-    Coordinate *coords = new Coordinate;
+    Coordinate coords;
     std::cout << "type a number X: ";
-    std::cin >> coords->x;
+    std::cin >> coords.x;
+
     std::cout << "type a number Y: ";
-    std::cin >> coords->y;
-    return *coords;
+    std::cin >> coords.y;
+
+	std::cin.get();
+    return coords;
 }
--- Lab1/Source/Main.cpp
+++ Lab1/Source/Main.cpp
@@ -1,34 +1,50 @@
 //Fil for implementation of the main-function
 #include <iostream>
+#include <string>
 #include "Functions.hpp"
 
 int main()
 {
     Map<std::uint16_t> map;
     Map<bool> booleanMap;
-    int TreasureFound = 0;
-    Coordinate *input = new Coordinate;
+	int TreasureFound = 0;
+	int moveCounter = 0;
+    Coordinate input;
+	std::uint16_t height = 10;
+	std::uint16_t width = 10;
 
-    CreateMap(map, booleanMap, 10, 10);
+    CreateMap(map, booleanMap, width, height);
 
     while (TreasureFound < 3)
     {
         PrintMap(map, booleanMap);
-        *input = InputCoordinates();
-        
-        if (map[input->x][input->y] == 0)
-        {
-            map[input->x][input->y] = map.size();
-            UpdateMap(map);
-            std::cout << "You found a treasure!" << std::endl;
-            TreasureFound++;
-            getchar();
-        }
-        
-        booleanMap[input->x][input->y] = true;
+        input = InputCoordinates();
 
-        //clear screen;
+		if (!std::cin.fail() && input.x < map[0].size() && input.y < map.size() && input.x >= 0 && input.y >= 0 && booleanMap[input.y][input.x] == false)
+		{
+			if (map[input.y][input.x] == 0)
+			{
+				map[input.y][input.x] = map.size();
+				UpdateMap(map);
+				std::cout << "You found a treasure!" << std::endl;
+				TreasureFound++;
+				getchar();
+			}
+
+			booleanMap[input.y][input.x] = true;
+			moveCounter++;
+		}
+
+		else
+		{
+			std::cin.clear();
+			std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+			std::cout << "\nInvalid input";	
+			getchar();
+		}
     }
+
+	std::cout << "it took you " << moveCounter << " tries to win";
     getchar();
 
     return 0;
