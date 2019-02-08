e35ff7f6589afcb2f2c1a4827059fb2d9ed2f9e1
Miheel <mikaelleuf@gmail.com>
2019-02-08 22:45:09 +0100

    update change from Pythagorean distance to jump distance

--- Lab1/Source/Functions.cpp
+++ Lab1/Source/Functions.cpp
@@ -5,6 +5,7 @@
 
 void ResetMap(Map<std::uint16_t> &map)
 {
+    const std::uint16_t maxValue = std::numeric_limits<std::uint16_t>::max();
     // Local helper function to reset all non treasure positions on the map to the maximum value
     // so that updateposition can then give them their correct values based on the remaining treasures
     for (std::uint16_t yPos = 0; yPos < map.size(); yPos++)
@@ -13,7 +14,7 @@ void ResetMap(Map<std::uint16_t> &map)
         {
             if (map[yPos][xPos] != 0)
             {
-                map[yPos][xPos] = static_cast<std::uint16_t>(map.size());
+                map[yPos][xPos] = maxValue; // fix
             }
         }
     }
@@ -22,65 +23,44 @@ void ResetMap(Map<std::uint16_t> &map)
 void UpdateFromPosition(Map<std::uint16_t> &map, std::uint16_t xPos, std::uint16_t yPos)
 {
     // Local helper function to update the map based on a specific position indicated by xPos and yPos
-	int lenPoint = 0;
-	int x1 = 0;
-	int y1= 0;
-    int x2 = 0;
-	int y2 = 0;
-
     for (std::uint16_t i = 0; i < map.size(); i++)
     {
         for (std::uint16_t j = 0; j < map[0].size(); j++)
-        {			    
-			if (map[i][j] != 0)
-			{
-				//Finds the distance between the treasures and all the other point with the distance folmula
-                x1 = i - yPos;
-                y1 = j - xPos;
+        {
+            if (map[i][j] != 0)
+            {
+                //jump distance to find the distance of adjacent horizontal, vertical and diagonal places in the map
+                int yLen = abs(yPos - i);
+                int xLen = abs(xPos - j);
+                int distanceFromTreasure = std::max(xLen, yLen);
 
-    			x2 = (int)std::pow(x1, 2);
-    			y2 = (int)std::pow(y1, 2);
-
-    			lenPoint = (int)std::sqrt(x2 + y2);
-
-				if(map[i][j] > lenPoint)
-				{						
-					map[i][j] = lenPoint;
-				}
-			}		
+                if (distanceFromTreasure < map[i][j])
+                {
+                    map[i][j] = distanceFromTreasure;
+                }
+            }
         }
     }
 }
 
 void CreateMap(Map<std::uint16_t> &map, Map<bool> &takenSpaces, std::uint16_t width, std::uint16_t height)
 {
-	//Creates a 2d vector based on with and height
-	map.resize(height, Column<std::uint16_t>(width, 1));
-	takenSpaces.resize(height, Column<bool>(width, false));
-    /*for (std::uint16_t i = 0; i < height; i++)
-    {
-        map.emplace_back();
-        takenSpaces.emplace_back();
+    //Creates a 2d vector based on with and height
+    map.resize(height, Column<std::uint16_t>(width, 1));
+    takenSpaces.resize(height, Column<bool>(width, false));
 
-        for (std::uint16_t j = 0; j < width; j++)
-        {
-            map.back().push_back(1);
-            takenSpaces.back().push_back(false);
-        }
-    }*/
-
-	//Places 3 treasures on the map 
+    //Places 3 treasures on the map
     srand(static_cast<unsigned>(time(NULL)));
     for (int i = 0; i < 3;)
     {
         int x = rand() % width;
         int y = rand() % height;
 
-		while (map[y][x] != 0)
-		{
-			map[y][x] = 0;
-			i++;
-		}
+        if (map[y][x] != 0)
+        {
+            map[y][x] = 0;
+            i++;
+        }
     }
 
     UpdateMap(map);
@@ -88,7 +68,7 @@ void CreateMap(Map<std::uint16_t> &map, Map<bool> &takenSpaces, std::uint16_t wi
 
 void UpdateMap(Map<std::uint16_t> &map)
 {
-	//Finds the locatios of the treasures and calls for UpdateFromPosition at the pos of the treasures
+    //Finds the locatios of the treasures and calls for UpdateFromPosition at the pos of the treasures
     ResetMap(map);
 
     for (std::uint16_t yPos = 0; yPos < map.size(); yPos++)
@@ -98,38 +78,38 @@ void UpdateMap(Map<std::uint16_t> &map)
             if (map[yPos][xPos] == 0)
             {
                 UpdateFromPosition(map, xPos, yPos);
-			}
+            }
         }
     }
 }
 
-const void erase()
+void erase()
 {
 #if defined _WIN32
-	system("cls");
+    system("cls");
 #else
-	system("clear");
+    system("clear");
 #endif
 }
 
 void PrintMap(Map<std::uint16_t> &map, Map<bool> &takenSpaces)
 {
-	//clears the screen and print out the map
-	erase();
+    //clears the screen and print out the map
+    erase();
 
     for (std::uint16_t yPos = 0; yPos < map.size(); yPos++)
     {
         for (std::uint16_t xPos = 0; xPos < map[0].size(); xPos++)
         {
-            if (takenSpaces[yPos][xPos] == true)
-            {
-                std::cout << " " << map[yPos][xPos] << " ";
-            }
+            //if (takenSpaces[yPos][xPos] == true)
+            // {
+            std::cout << " " << map[yPos][xPos] << " ";
+            // }
 
-            else
-            {
-                std::cout << " * ";
-            }
+            // else
+            // {
+            //     std::cout << " * ";
+            // }
         }
 
         std::cout << std::endl;
@@ -145,6 +125,6 @@ Coordinate InputCoordinates()
     std::cout << "type a number Y: ";
     std::cin >> coords.y;
 
-	std::cin.get();
+    std::cin.clear(); // fix
     return coords;
 }
